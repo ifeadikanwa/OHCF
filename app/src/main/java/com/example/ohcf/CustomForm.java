022672d0bd88da00b6386d195920aa75725fe667
ifeadikanwa <ezeifyyy@gmail.com>
2020-10-17 17:51:28 -0400

    created CustomForm model class for firestore

---
+++ app/src/main/java/com/example/ohcf/CustomForm.java
@@ -0,0 +1,220 @@
+package com.example.ohcf;
+
+public class CustomForm {
+    String last_name;
+    String first_name;
+    String middle_initial;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String home_phone;
+    String social_security;
+    String date_of_birth;
+    String age;
+    String sex;
+    String marital_status;
+    String drivers_license_num;
+    String employee_name;
+    String employee_address;
+    String work_phone;
+    String email_address;
+    String cell_phone;
+    String emergency_phone;
+    String emergency_contact_name;
+
+    public CustomForm() {
+    }
+
+    public CustomForm(String last_name, String first_name, String middle_initial, String address, String city, String state, String zip, String home_phone, String social_security, String date_of_birth, String age, String sex, String marital_status, String drivers_license_num, String employee_name, String employee_address, String work_phone, String email_address, String cell_phone, String emergency_phone, String emergency_contact_name) {
+        this.last_name = last_name;
+        this.first_name = first_name;
+        this.middle_initial = middle_initial;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.home_phone = home_phone;
+        this.social_security = social_security;
+        this.date_of_birth = date_of_birth;
+        this.age = age;
+        this.sex = sex;
+        this.marital_status = marital_status;
+        this.drivers_license_num = drivers_license_num;
+        this.employee_name = employee_name;
+        this.employee_address = employee_address;
+        this.work_phone = work_phone;
+        this.email_address = email_address;
+        this.cell_phone = cell_phone;
+        this.emergency_phone = emergency_phone;
+        this.emergency_contact_name = emergency_contact_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getMiddle_initial() {
+        return middle_initial;
+    }
+
+    public void setMiddle_initial(String middle_initial) {
+        this.middle_initial = middle_initial;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getHome_phone() {
+        return home_phone;
+    }
+
+    public void setHome_phone(String home_phone) {
+        this.home_phone = home_phone;
+    }
+
+    public String getSocial_security() {
+        return social_security;
+    }
+
+    public void setSocial_security(String social_security) {
+        this.social_security = social_security;
+    }
+
+    public String getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(String date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getMarital_status() {
+        return marital_status;
+    }
+
+    public void setMarital_status(String marital_status) {
+        this.marital_status = marital_status;
+    }
+
+    public String getDrivers_license_num() {
+        return drivers_license_num;
+    }
+
+    public void setDrivers_license_num(String drivers_license_num) {
+        this.drivers_license_num = drivers_license_num;
+    }
+
+    public String getEmployee_name() {
+        return employee_name;
+    }
+
+    public void setEmployee_name(String employee_name) {
+        this.employee_name = employee_name;
+    }
+
+    public String getEmployee_address() {
+        return employee_address;
+    }
+
+    public void setEmployee_address(String employee_address) {
+        this.employee_address = employee_address;
+    }
+
+    public String getWork_phone() {
+        return work_phone;
+    }
+
+    public void setWork_phone(String work_phone) {
+        this.work_phone = work_phone;
+    }
+
+    public String getEmail_address() {
+        return email_address;
+    }
+
+    public void setEmail_address(String email_address) {
+        this.email_address = email_address;
+    }
+
+    public String getCell_phone() {
+        return cell_phone;
+    }
+
+    public void setCell_phone(String cell_phone) {
+        this.cell_phone = cell_phone;
+    }
+
+    public String getEmergency_phone() {
+        return emergency_phone;
+    }
+
+    public void setEmergency_phone(String emergency_phone) {
+        this.emergency_phone = emergency_phone;
+    }
+
+    public String getEmergency_contact_name() {
+        return emergency_contact_name;
+    }
+
+    public void setEmergency_contact_name(String emergency_contact_name) {
+        this.emergency_contact_name = emergency_contact_name;
+    }
+}
