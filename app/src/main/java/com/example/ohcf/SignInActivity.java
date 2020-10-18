5daaaff4f32c064c907eae8fc25fd8191b1b5b10
ifeadikanwa <ezeifyyy@gmail.com>
2020-10-18 05:05:41 -0400

    SignInActivity complete

---
+++ app/src/main/java/com/example/ohcf/SignInActivity.java
@@ -0,0 +1,36 @@
+package com.example.ohcf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        ImageButton register = findViewById(R.id.register_button);
+        ImageButton login = findViewById(R.id.login_button);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
