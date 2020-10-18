940817ad0148b36dbe56ec6146617789cf4fb639
yuanxinwang <alexyuanxinwang@gmail.com>
2020-10-18 10:36:45 -0400

    Longest Common Substring

--- app/src/main/java/com/example/ohcf/AppointmentPhotoFragment.java
+++ app/src/main/java/com/example/ohcf/AppointmentPhotoFragment.java
@@ -141,7 +141,34 @@ public class AppointmentPhotoFragment extends Fragment {
             }
         });
     }
+    int LCSubStr(char X[], char Y[], int m, int n)
+    {
+        // Create a table to store lengths of longest common suffixes of
+        // substrings. Note that LCSuff[i][j] contains length of longest
+        // common suffix of X[0..i-1] and Y[0..j-1]. The first row and
+        // first column entries have no logical meaning, they are used only
+        // for simplicity of program
+        int LCStuff[][] = new int[m + 1][n + 1];
+        int result = 0;  // To store length of the longest common substring
 
+        // Following steps build LCSuff[m+1][n+1] in bottom up fashion
+        for (int i = 0; i <= m; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                if (i == 0 || j == 0)
+                    LCStuff[i][j] = 0;
+                else if (X[i - 1] == Y[j - 1])
+                {
+                    LCStuff[i][j] = LCStuff[i - 1][j - 1] + 1;
+                    result = Integer.max(result, LCStuff[i][j]);
+                }
+                else
+                    LCStuff[i][j] = 0;
+            }
+        }
+        return result;
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data)
@@ -177,7 +204,14 @@ public class AppointmentPhotoFragment extends Fragment {
                                                 String lineText = line.getText().toLowerCase();
                                                 Log.d("forminfo", lineText);
                                                 for(String formField: formString) {
-                                                    if (lineText.contains(formField.toLowerCase()) && formField.length() > 2) {
+                                                    int longestCommonSubstring = LCSubStr(lineText.toCharArray(),
+                                                            formField.toLowerCase().toCharArray(), lineText.length(), formField.length());
+                                                    if (lineText.contains(formField.toLowerCase()) && formField.length() > 2
+                                                    && lineText.length() - formField.length() < 2) {
+                                                        args.putString(formField, lineText);
+                                                        continue;
+                                                    }
+                                                    if (formField.length() > 5 && longestCommonSubstring > (formField.length() / 5 * 4)) {
                                                         args.putString(formField, lineText);
                                                         continue;
                                                     }
